3b6d384d157846cfa536339d64a6ec5a63f027a2
Li <ekinasm@gmail.com>
2016-03-17 17:12:45 +0800

    2016年03月17日 添加heatmap.html

--- hello.py
+++ hello.py
@@ -21,5 +21,16 @@ def hello():
 	#entries = [dict(longitude=row[0], latitude=row[1],count=int(row[2]) for row in cur.fetchall()]
 	return render_template('hello.html', entries=cur.fetchall())
 
+@app.route('/heatmap')
+def heatmap():
+	db = MySQLdb.connect(host="10.19.251.50",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="sxcloud",  # your password
+                     db="test")        # name of the data base
+
+	cur = db.cursor()
+	cur.execute("SELECT * FROM heat_map limit 10")
+	return render_template('heatmap.html',entries=cur.fetchall())
+
 if __name__ == '__main__':
     app.run(debug=True)
